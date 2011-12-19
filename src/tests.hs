808e648adeffd3b85c0118a4720cb0826ae574cb
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2011-12-19 12:28:08 +0100

    Introduce test-framework.

--- src/tests.hs
+++ src/tests.hs
@@ -1,3 +1,12 @@
 module Main where
 
-main = putStrLn "Good."
+import Test.HUnit ((@=?))
+import Test.QuickCheck hiding ((.&.))
+import Test.Framework (Test, defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.Framework.Providers.HUnit (testCase)
+
+main = defaultMain tests
+
+tests =
+  [  {- testCase "foo" (1 @=? 1) -} ]
