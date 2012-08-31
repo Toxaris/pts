b7403ef2c1a447b5247f5879e6fe22c37a898308
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2012-08-31 23:44:53 +0200

    Expose some functions for light-weight transformations.

--- src-lib/PTS/Transform.hs
+++ src-lib/PTS/Transform.hs
@@ -1,6 +1,10 @@
 module PTS.Transform
   (  transform
   ,  typeOf
+  ,  sortOf
+  ,  structure
+  ,  Sort (Term, Type)
+  ,  TermStructure (..)
   )  where
 
 import Control.Monad.Environment (runEnvironmentT)
@@ -10,16 +14,26 @@ import Control.Monad.Trans (liftIO)
 
 import Parametric.Error (showErrors)
 
-import PTS.AST (TypedTerm, typeOf)
+import PTS.AST (TypedTerm, typeOf, structure, TermStructure (..))
 import PTS.Core (typecheck)
 import PTS.Options (defaultOptions)
 import PTS.Parser (parseTerm)
 import PTS.Pretty ()
+import PTS.Constants (C (C))
 
 import System.IO (hPutStrLn, stderr)
 import System.Exit (exitSuccess, exitFailure)
 
 import Tools.Errors (runErrorsT)
+
+data Sort
+  = Term
+  | Type
+
+sortOf :: TypedTerm -> Sort
+sortOf t = case structure (typeOf (typeOf t)) of
+  Const (C 1) -> Term
+  Const (C 2) -> Type
 
 run p = do
   result <- runErrorsT (p `runConsoleLogT` False) `runReaderT` defaultOptions
