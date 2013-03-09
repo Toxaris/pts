22c10a73d724a29201782ed7c0fccc3251542e7a
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-03-09 16:25:29 +0100

    Get as many errors as possible
    We should run as many other checks as possible even if tests fail.

--- allTests.sh
+++ allTests.sh
@@ -1,20 +1,21 @@
-#!/bin/sh -vx
+#!/bin/sh -x
 
 set -e
 
 cabal clean
 
-# Ignore failures from cabal check - for now.
 set +e
+# Ignore failures from these tasks - for now.
 cabal check
 set -e
 
 cabal install --only-dependencies
 cabal configure --enable-tests
 cabal build
-dist/build/tests/tests --hide-successes --maximum-generated-tests=10000 --jxml=junit-log.xml
 
 cabal sdist
+
+dist/build/tests/tests --hide-successes --maximum-generated-tests=10000 --jxml=junit-log.xml
 
 nameBase=`runhaskell src-tools/package-info.hs --package`
 cabal install dist/$nameBase.tar.gz --enable-tests
