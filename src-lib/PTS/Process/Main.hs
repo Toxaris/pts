59d79e022785b914ebeff8a6b2d8e798c5fea9bb
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-07-13 15:19:07 +0200

    Create PTS.Interactive and DEVELOPMENT.md for REPL
    Try to document and improve the REPL workflow, and add helpers for that.
    
    For now, we just support easily creating terms.

--- src-lib/PTS/Process/Main.hs
+++ src-lib/PTS/Process/Main.hs
@@ -51,5 +51,12 @@ processJob :: (Functor m, MonadIO m, MonadErrors [PTSError] m, MonadState (Map.M
 processJob (opt, file) = do
   let path = optPath opt
   file <- liftIO (findFile path file) >>= maybe (fail ("file not found: " ++ file)) return
-  mod <- checkAssertions (runReaderT (runConsoleLogT (processFile file) (optDebugType opt)) opt)
+  mod <- processFile' file opt
   return ()
+
+processFile' file opt =
+  checkAssertions (runReaderT (runConsoleLogT (processFile file) (optDebugType opt)) opt)
+
+processFileSimple
+  :: FilePath -> IO (Either [PTSError] (Maybe (Module M)))
+processFileSimple f = runErrorsT . runMainState $ processFile' f defaultOptions
