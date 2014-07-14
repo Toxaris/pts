Toxaris/pts

commit 5ba040a19d5bcc57ab521a2920cd8b373387670a

Return annotated term from type checking.

src-lib/PTS/Instances.hs
@@ -11,7 +11,8 @@ import Data.List
 
 import Parametric.Pretty
 
-
+import PTS.AST
+import PTS.Constants
 
 -- This type defines a specific pure type system, see Barendregt
 --
@@ -20,111 +21,116 @@ import Parametric.Pretty
 -- axioms, sorts, relations, nat, constants parser
 
 data PTS = PTS
-  {  axioms :: C -> Maybe C
+  {  axioms :: C -> Maybe TypedTerm
   ,  sorts :: C -> Bool
-  ,  relations :: C -> C -> Maybe C
+  ,  relations :: C -> C -> Maybe TypedTerm
   }
 
--- constants
-newtype C = C Int
-  deriving (Eq, Data, Typeable)
-
-nat :: C
-nat = C 0
-
-star :: C
-star = C 1
-
-box :: C
-box = C 2
+notExpressible = MkTypedTerm (Const (C (negate 1))) notExpressible
 
 -- some specific pure type systems
 
 -- simply typed lambda calculus
 simplytyped :: PTS
 simplytyped = PTS axioms sorts relations where
-  axioms (C 0) = Just (C 1)
+  axioms (C 0) = Just (typedStar)
   axioms (C _) = Nothing
 
   sorts (C 1) = True
   sorts (C _) = False
 
-  relations (C 1) (C 1) = Just (C 1)
+  relations (C 1) (C 1) = Just typedStar
   relations (C _) (C _) = Nothing
+
+  typedStar = MkTypedTerm (Const star) notExpressible
 
 fomega :: PTS
 fomega = PTS axioms sorts relations where
-  axioms (C 0) = Just (C 1)
-  axioms (C 1) = Just (C 2)
+  axioms (C 0) = Just (typedStar)
+  axioms (C 1) = Just (typedBox)
   axioms (C _) = Nothing
 
   sorts (C 1) = True
   sorts (C 2) = True
   sorts (C _) = False
 
-  relations (C 1) (C 1) = Just (C 1)
+  relations (C 1) (C 1) = Just (typedStar)
   relations (C 1) (C 2) = Nothing
-  relations (C 2) (C 1) = Just (C 1)
-  relations (C 2) (C 2) = Just (C 2)
+  relations (C 2) (C 1) = Just (typedStar)
+  relations (C 2) (C 2) = Just (typedBox)
   relations (C _) (C _) = Nothing
+
+  typedStar = MkTypedTerm (Const star) typedBox
+  typedBox  = MkTypedTerm (Const box) notExpressible
+
 
 -- calculus of construction
 coc :: PTS
 coc = PTS axioms sorts relations where
-  axioms (C 0) = Just (C 1)
-  axioms (C 1) = Just (C 2)
+  axioms (C 0) = Just typedStar
+  axioms (C 1) = Just typedStar
   axioms (C _) = Nothing
 
   sorts (C 1) = True
   sorts (C 2) = True
   sorts (C _) = False
 
-  relations (C 1) (C 1) = Just (C 1)
-  relations (C 1) (C 2) = Just (C 2)
-  relations (C 2) (C 1) = Just (C 1)
-  relations (C 2) (C 2) = Just (C 2)
+  relations (C 1) (C 1) = Just typedStar
+  relations (C 1) (C 2) = Just typedBox
+  relations (C 2) (C 1) = Just typedStar
+  relations (C 2) (C 2) = Just typedBox
   relations (C _) (C _) = Nothing
+
+  typedStar = MkTypedTerm (Const star) typedBox
+  typedBox  = MkTypedTerm (Const box) notExpressible
 
 -- lambda* with * : *
 lambdastar :: PTS
 lambdastar = PTS axioms sorts relations where
-  axioms (C 0) = Just (C 1)
-  axioms (C 1) = Just (C 1)
+  axioms (C 0) = Just typedStar
+  axioms (C 1) = Just typedStar
   axioms (C _) = Nothing
 
   sorts (C 1) = True
   sorts (C _) = False
 
-  relations (C 1) (C 1) = Just (C 1)
+  relations (C 1) (C 1) = Just typedStar
   relations (C _) (C _) = Nothing
+
+  typedStar = MkTypedTerm (Const star) typedStar
 
 -- fomega* with ** : **
 fomegastar :: PTS
 fomegastar = PTS axioms sorts relations where
-  axioms (C 0) = Just (C 1)
-  axioms (C 1) = Just (C 2)
-  axioms (C 2) = Just (C 2)
+  axioms (C 0) = Just typedStar
+  axioms (C 1) = Just typedBox
+  axioms (C 2) = Just typedBox
   axioms (C _) = Nothing
 
   sorts (C 1) = True
   sorts (C 2) = True
   sorts (C _) = False
 
-  relations (C 1) (C 1) = Just (C 1)
+  relations (C 1) (C 1) = Just typedStar
   relations (C 1) (C 2) = Nothing
-  relations (C 2) (C 1) = Just (C 1)
-  relations (C 2) (C 2) = Just (C 2)
+  relations (C 2) (C 1) = Just typedStar
+  relations (C 2) (C 2) = Just typedBox
   relations (C _) (C _) = Nothing
+
+  typedStar = MkTypedTerm (Const star) typedBox
+  typedBox  = MkTypedTerm (Const box) typedBox
 
 -- FOmega with an infinite hierarchy of universes
 -- We call this language FOmegaOmega
 fomegaomega :: PTS
 fomegaomega = PTS axioms sorts relations where
-  axioms (C n) = Just (C (succ n))
+  axioms (C n) = Just (typed (succ n))
 
   sorts (C 0) = False
   sorts (C n) = True
 
   relations (C 0) (C _) = Nothing
   relations (C _) (C 0) = Nothing
-  relations (C a) (C b) = if a >= b then Just (C b) else Nothing
+  relations (C a) (C b) = if a >= b then Just (typed b) else Nothing
+
+  typed n = MkTypedTerm (Const (C n)) (typed (succ n))

pts.cabal
@@ -39,6 +39,7 @@ Library
                        PTS.Main,
                        PTS.Substitution,
                        PTS.QuasiQuote,
+                       PTS.Constants,
                        Parametric.Parser.Error,
                        Parametric.Parser,
                        PTS.Parser,

src-lib/PTS/AST.hs
@@ -20,6 +20,7 @@ module PTS.AST
   , mkUnquote
   , freshvarl
   , handlePos
+  , typedHandlePos
   , C ()
   , evalOp
   , BinOp (..)
@@ -40,7 +41,7 @@ import System.IO.Unsafe (unsafePerformIO)
 
 import Parametric.AST (Name, Names, freshvarl)
 import Parametric.Error
-import PTS.Instances (C)
+import PTS.Constants (C)
 
 
 -- Syntax
@@ -128,6 +129,8 @@ mkPos p t          =  mkTerm (Pos p t)
 mkUnquote t        =  mkTerm (Unquote t)
 
 handlePos f p t = annotatePos p $ mkPos p <$> f t
+
+typedHandlePos f p t = annotatePos p $ (\t -> MkTypedTerm (Pos p t) (typeOf t)) <$> f t
 
 infixl 2 >>>
 (>>>) = flip (.)

src-lib/PTS/Algebra.hs
@@ -2,6 +2,7 @@ module PTS.Algebra
   ( PreAlgebra
   , Algebra
   , fold
+  , strip
   , allvars
   , allvarsAlgebra
   , freevars
@@ -22,10 +23,13 @@ type PreAlgebra alpha beta
 type Algebra alpha
   = PreAlgebra alpha alpha
 
-fold :: Algebra alpha -> Term -> alpha
+fold :: Structure term => Algebra alpha -> term -> alpha
 fold algebra term = algebra (fmap (fold algebra) (structure term))
 
-allvars :: Term -> Names
+strip :: Structure term => term -> Term
+strip t = fold MkTerm t
+
+allvars :: Structure term => term -> Names
 allvars t = fold allvarsAlgebra t
 
 allvarsAlgebra :: Algebra Names
@@ -49,10 +53,10 @@ freevarsAlgebra t = case t of
   Pos p t          ->  t
   _                ->  Set.empty
 
-freevars :: Term -> Names
+freevars :: Structure term => term -> Names
 freevars = fold freevarsAlgebra
 
-freshvar :: Term -> Name -> Name
+freshvar :: Structure term => term -> Name -> Name
 freshvar t x = freshvarl (freevars t) x
 
 -- instance Arrow PreAlgebra?

src-lib/PTS/Constants.hs
@@ -1,0 +1,18 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+module PTS.Constants where
+
+import Data.Typeable
+import Data.Data
+
+-- constants
+newtype C = C Int
+  deriving (Eq, Data, Typeable)
+
+nat :: C
+nat = C 0
+
+star :: C
+star = C 1
+
+box :: C
+box = C 2

src-lib/PTS/Core.hs
@@ -32,6 +32,7 @@ import PTS.Options
 import PTS.Pretty
 import PTS.Substitution
 import PTS.Evaluation
+import PTS.Constants
 
 import Text.Show (Show (show))
 
@@ -84,7 +85,7 @@ lookupType x = do
 --   trace ((show d) ++ (ndots d) ++ (show ctx) ++ " |- "++(show t) ++ " : ???") False
 
 -- safe bind
-safebind :: MonadEnvironment Name (Value, Term) m => Name -> Term -> Term -> (Name -> Term -> m a) -> m a
+safebind :: MonadEnvironment Name (Value, TypedTerm) m => Name -> TypedTerm -> Term -> (Name -> Term -> m a) -> m a
 safebind x t b f = do
   result <- lookupType x
   case result of
@@ -95,7 +96,7 @@ safebind x t b f = do
       let nx = freshvarl (freevars b `Set.union` vars) x
       bind nx (ResidualVar nx, t) (f nx (subst b x (mkVar nx)))
 
-debug :: (MonadEnvironment Name (Value, Term) m, MonadLog m) => String -> Term -> m Term -> m Term
+debug :: (MonadEnvironment Name (Value, TypedTerm) m, MonadLog m) => String -> Term -> m TypedTerm -> m TypedTerm
 debug n t result = do
   enter n
   ctx <- getEnvironment
@@ -112,7 +113,7 @@ normalizeToSort t' t context info = do
   pts <- asks optInstance
   env <- getEnvironment
 
-  case structure (nbe env t') of
+  case structure (nbe env (strip t')) of
     Const s | sorts pts s  ->  return s
     _                      ->  prettyFail $ msgNotProperType context info t t'
 
@@ -123,11 +124,13 @@ msgNotProperType context info t t'
     sep [text "Type:", nest 2 (pretty 0 t')])
 
 normalizeToSame s' t' s t context info1 info2 = do
+  let stripS' = strip s'
+  let stripT' = strip t'
   env <- getEnvironment
-  if (equivTerm env s' t')
+  if (equivTerm env (stripS') (stripT'))
       then return ()
-      else let  s'' = nbe env s'
-                t'' = nbe env t'
+      else let  s'' = nbe env (stripS')
+                t'' = nbe env (stripT')
              in prettyFail $ msgNotSame context info1 info2 s t s'' t''
 
 msgNotSame context info1 info2 s t s' t'
@@ -139,12 +142,13 @@ msgNotSame context info1 info2 s t s' t'
         sep [text "Type of" <+> info1 <> text ":", nest 2 (text s'')] $$
         sep [text "Type of" <+> info2 <> text ":", nest 2 (text t'')])
 
-normalizeToNat :: (Functor m, MonadEnvironment Name (Value, Term) m, MonadReader Options m, MonadErrors Errors m) => Term -> Term -> Doc -> Doc -> m Term
+normalizeToNat :: (MonadLog m, Functor m, MonadEnvironment Name (Value, TypedTerm) m, MonadReader Options m, MonadErrors Errors m) => TypedTerm -> TypedTerm -> Doc -> Doc -> m TypedTerm
 normalizeToNat t' t context info = do
+  let stripT' = stripT'
   env <- getEnvironment
-  if equivTerm env t' (mkConst nat)
-    then return (mkConst nat)
-    else let t'' = nbe env t'
+  if equivTerm env stripT' (mkConst nat)
+    then typecheck (mkConst nat)
+    else let t'' = nbe env stripT'
           in prettyFail $ msgNotNat context info t t'' nat
 
 msgNotNat context info t t' nat
@@ -156,7 +160,7 @@ msgNotNat context info t t' nat
 
 normalizeToPi t' t context info = do
   env <- getEnvironment
-  let t'' = nbe env t' in
+  let t'' = nbe env (strip t') in
      case structure t'' of
        result@(Pi _ _ _)  ->  return result
        _                  ->  prettyFail $ msgNotPi context info t t''
@@ -167,7 +171,7 @@ msgNotPi context info t t'
     sep [info <> text ":", nest 2 (pretty 0 t)] $$
     sep [text "Type of" <+> info <> text ":", nest 2 (pretty 0 t')])
 
-typecheck :: (MonadEnvironment Name (Value, Term) m, MonadReader Options m, MonadErrors Errors m, Functor m, MonadLog m) => Term -> m Term
+typecheck :: (MonadEnvironment Name (Value, TypedTerm) m, MonadReader Options m, MonadErrors Errors m, Functor m, MonadLog m) => Term -> m TypedTerm
 --typecheck p d ctx t | mytrace d ctx t = undefined
 
 typecheck t = case structure t of
@@ -175,7 +179,7 @@ typecheck t = case structure t of
   Const c -> debug "TypeConst" t $ do
     pts <- asks optInstance
     case axioms pts c of
-      Just t  ->  return (mkConst t)
+      Just t  ->  return (MkTypedTerm (Const c) t)
       _       ->  prettyFail $ text "Unknown constant:" <+> pretty 0 c
 
   -- start
@@ -183,77 +187,85 @@ typecheck t = case structure t of
     xt <- lookupType x
     case xt of
       Just xt -> do
-        s <- typecheck xt
-        normalizeToSort s xt (text "in variable") (text "as type of" <+> pretty 0 x)
-        return xt
+        -- s <- typecheck xt
+        -- normalizeToSort s xt (text "in variable") (text "as type of" <+> pretty 0 x)
+        return (MkTypedTerm (Var x) xt)
 
       Nothing ->
         fail $ "Unbound identifier: " ++ show x
 
   -- product
   Pi x a b -> debug "TypeFun" t $ do
-    s1 <- typecheck a
+    a'@(MkTypedTerm _ s1) <- typecheck a
     s1' <- normalizeToSort s1 a (text "in product type") (text "as domain")
 
-    safebind x a b $ \newx newb -> do
-      s2 <- typecheck newb
+    safebind x a' b $ \newx newb -> do
+      newb'@(MkTypedTerm _ s2) <- typecheck newb
       s2' <- normalizeToSort s2 newb (text "in product type") (text "as codomain")
 
       pts <- asks optInstance
       case relations pts s1' s2' of
-        Just s -> return (mkConst s)
+        Just s -> return (MkTypedTerm (Pi newx a' newb') s)
         Nothing -> prettyFail $ text "no relation" <+> pretty 0 s1' <+> text ":" <+> pretty 0 s2'
 
   -- application
   App t1 t2 -> debug "TypeApp" t $ do
-    tt1 <- typecheck t1
+    t1'@(MkTypedTerm _ tt1) <- typecheck t1
     Pi x a b <- normalizeToPi tt1 t1 (text "in application") (text "operator")
 
-    tt2 <- typecheck t2
+    -- TODO avoid rechecking
+    a' <- typecheck a
+    b' <- bind x (ResidualVar x, a') $
+            typecheck b
+
+    t2'@(MkTypedTerm _ tt2) <- typecheck t2
     normalizeToSame a tt2 (pretty 0 x) t2 (text "in application") (text "formal parameter") (text "actual parameter")
 
-    return (subst b x t2)
+    -- TODO get rid of subst?
+    return (MkTypedTerm (App t1' t2') (typedSubst b' x t2'))
 
   -- abstraction
   Lam x a b -> debug "TypeAbs" t $ do
-    s1  <- typecheck a
+    a'@(MkTypedTerm _ s1)  <- typecheck a
     s1' <- normalizeToSort s1 a (text "in lambda abstraction") (text "as type of" <+> pretty 0 x)
 
-    safebind x a b $ \newx newb -> do
-      tb  <- typecheck newb
+    safebind x a' b $ \newx newb -> do
+      newb'@(MkTypedTerm _ tb)  <- typecheck newb
       env <- getEnvironment
-      let tb' = nbe env tb
-      s2  <- typecheck tb'
+      let tb' = nbe env (strip tb)
+      tb''@(MkTypedTerm _ s2)  <- typecheck tb'
       s2' <- normalizeToSort s2 tb' (text "in lambda abstraction") (text "as type of body")
 
       pts <- asks optInstance
-      maybe (fail $ "no relation " ++ show s1 ++ " : " ++ show s2) return (relations pts s1' s2')
+      s3 <- maybe (fail $ "no relation " ++ show s1 ++ " : " ++ show s2) return (relations pts s1' s2')
 
-      return (mkPi newx a tb')
+      return (MkTypedTerm (Lam newx a' newb') (MkTypedTerm (Pi newx a' tb'') s3))
 
   -- Nat
   Nat i -> debug "TypeNat" t $ do
-    return (mkConst nat)
+    nat' <- typecheck (mkConst nat)
+    return (MkTypedTerm (Nat i) nat')
 
   -- NatOp
   NatOp i f t1 t2 -> debug "TypeNatOp" t $ do
-    tt1 <- typecheck t1
-    normalizeToNat tt1 t1 (text "in" <+> pretty 0 i) (text "first argument of" <+> pretty 0 i)
-    tt2 <- typecheck t2
-    normalizeToNat tt2 t2 (text "in" <+> pretty 0 i) (text "second argument of" <+> pretty 0 i)
+    t1'@(MkTypedTerm _ tt1) <- typecheck t1
+    normalizeToNat tt1 t1' (text "in" <+> pretty 0 i) (text "first argument of" <+> pretty 0 i)
+    t2'@(MkTypedTerm _ tt2) <- typecheck t2
+    result <- normalizeToNat tt2 t2' (text "in" <+> pretty 0 i) (text "second argument of" <+> pretty 0 i)
+    return (MkTypedTerm (NatOp i f t1' t2') result)
 
   -- IfZero
   IfZero t1 t2 t3 -> debug "TypeIfZero" t $ do
-    tt1 <- typecheck t1
-    normalizeToNat tt1 t1 (text "in if0") (text "condition")
-    tt2 <- typecheck t2
-    tt3 <- typecheck t3
-    normalizeToSame tt2 tt3 t2 t3 (text "in if0") (text "then branch") (text "else branch")
-    return tt2
+    t1'@(MkTypedTerm _ tt1) <- typecheck t1
+    normalizeToNat tt1 t1' (text "in if0") (text "condition")
+    t2'@(MkTypedTerm _ tt2) <- typecheck t2
+    t3'@(MkTypedTerm _ tt3) <- typecheck t3
+    normalizeToSame tt2 tt3 t2' t3' (text "in if0") (text "then branch") (text "else branch")
+    return (MkTypedTerm (IfZero t1' t2' t3') tt2)
 
   -- Position information
   Pos p t -> do
     -- trace ("Start: "++(show ctx) ++ " |- " ++ (show t) ++ " : ???") (return ())
-    x <- handlePos typecheck p t
+    x <- typedHandlePos typecheck p t
     -- trace ("End: "++(show ctx) ++ " |- " ++ (show t) ++ " : "++(show x)) (return ())
     return x

src-lib/PTS/Evaluation.hs
@@ -8,7 +8,7 @@ import qualified Data.Set as Set
 import Control.Monad.State
 
 type Env = [(Name, Value)]
-type Env' = [(Name, (Value, Term))]
+type Env' = [(Name, (Value, TypedTerm))]
 
 envToNames :: Env -> Names
 envToNames env = Set.fromList (map fst env)

src-lib/PTS/Main.hs
@@ -24,6 +24,7 @@ import PTS.Options
 import PTS.Parser
 import PTS.Substitution
 import PTS.Evaluation
+import PTS.Algebra
 
 import qualified Data.Set as Set
 
@@ -59,12 +60,12 @@ processJobs jobs = do
     then exitSuccess
     else exitFailure
 
-processJob :: (Functor m, MonadIO m, MonadErrors [FOmegaError] m, MonadState [(Name, (Value, Term))] m) => (Options, FilePath) -> m ()
+processJob :: (Functor m, MonadIO m, MonadErrors [FOmegaError] m, MonadState [(Name, (Value, TypedTerm))] m) => (Options, FilePath) -> m ()
 processJob (opt, file) = do
   liftIO $ putStrLn $ "process file " ++ file
   runReaderT (runConsoleLogT (processFile file) (optDebugType opt)) opt
 
-processFile :: (Functor m, MonadErrors [FOmegaError] m, MonadReader Options m, MonadState [(Name, (Value, Term))] m, MonadIO m, MonadLog m) => FilePath -> m ()
+processFile :: (Functor m, MonadErrors [FOmegaError] m, MonadReader Options m, MonadState [(Name, (Value, TypedTerm))] m, MonadIO m, MonadLog m) => FilePath -> m ()
 processFile file = do
   text <- liftIO (readFile file)
   text <- deliterate text
@@ -83,7 +84,7 @@ processStmt (Term t) = recover () $ do
   output (nest 2 (sep [text "original term:", nest 2 (pretty 0 t)]))
   whenOption optShowFullTerms $ output (nest 2 (sep [text "full term:", nest 2 (pretty 0 t)]))
   env <- get
-  q <- runEnvironmentT (typecheck t) env
+  MkTypedTerm _ q <- runEnvironmentT (typecheck t) env
   output (nest 2 (sep [text "type:", nest 2 (pretty 0 q)]))
   let x = nbe env t
   output (nest 2 (sep [text "value:", nest 2 (pretty 0 x)]))
@@ -95,7 +96,7 @@ processStmt (Bind n Nothing t) = recover () $ do
   output (nest 2 (sep [text "original term:", nest 2 (pretty 0 t)]))
   env <- get
   whenOption optShowFullTerms $ output (nest 2 (sep [text "full term:", nest 2 (pretty 0 t)]))
-  q <- runEnvironmentT (typecheck t) env
+  MkTypedTerm _ q <- runEnvironmentT (typecheck t) env
   output (nest 2 (sep [text "type:", nest 2 (pretty 0 q)]))
   let v = evalTerm env t
   modify ((n, (v, q)) :)
@@ -116,20 +117,20 @@ processStmt (Bind n (Just t') t) = recover () $ do
   env <- get
 
   -- typecheck type
-  q' <- runEnvironmentT (typecheck t'') env
-  case structure (nbe env q') of
+  MkTypedTerm _ q' <- runEnvironmentT (typecheck t'') env
+  case structure (nbe env (strip q')) of
     Const _ -> return ()
     _       -> prettyFail $  text "Type error in top-level binding of " <+> pretty 0 n
                          $$ text "  expected:" <+> text "constant"
                          $$ text "     found:" <+> pretty 0 q'
 
   -- typecheck body
-  q <- runEnvironmentT (typecheck t) env
+  MkTypedTerm _ q <- runEnvironmentT (typecheck t) env
 
   -- compare specified and actual type
-  if equivTerm env q t''
-    then output (nest 2 (sep [text "type:", nest 2 (pretty 0 t' )]))
-    else let (expected, given) = showDiff 0 (diff (nbe env t'' ) (nbe env q)) in
+  if equivTerm env (strip q) t''
+    then output (nest 2 (sep [text "type:", nest 2 (pretty 0 (strip t') )]))
+    else let (expected, given) = showDiff 0 (diff (nbe env t'' ) (nbe env (strip q))) in
            prettyFail $ text "Type mismatch in top-level binding of" <+> pretty 0 n
                      $$ text "  specified type:" <+> pretty 0 t'
                      $$ text "     normal form:" <+> text expected

commit 570b060f895a2ce3a5c08e58264b53cabb09fc54

Fix bad typo in COC instance
Introduced in 5ba040a19d5bcc57ab521a2920cd8b373387670a.

src-lib/PTS/Instances.hs
@@ -226,7 +226,7 @@ lamc = PTS sorts axioms relations name description where
   sorts (C _) = False
 
   axioms (C 0) = Just typedStar
-  axioms (C 1) = Just typedStar
+  axioms (C 1) = Just typedBox
   axioms (C _) = Nothing
 
   relations (C 1) (C 1) = Just typedStar