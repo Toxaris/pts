a7cac000ca74340bddb1454ed0073200fbf669ca
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2014-08-07 22:54:41 +0200

    Use Telescope type in desugarArgs signature.

--- src-lib/PTS/Syntax/Telescope.hs
+++ src-lib/PTS/Syntax/Telescope.hs
@@ -14,7 +14,7 @@ type Telescope t = [([Name], t)]
 -- to a series of nested single argument binders:
 --
 -- > lambda x1 : e1 . lambda x2 : e2 . lambda x3 : e2 . e
-desugarArgs :: (Name -> a -> b -> b) -> [([Name], a)] -> b -> b
+desugarArgs :: (Name -> a -> b -> b) -> Telescope a -> b -> b
 desugarArgs mk [] body = body
 desugarArgs mk (([], _) : args) body = desugarArgs mk args body
 desugarArgs mk (((n : ns), t) : args) body = mk n t (desugarArgs mk ((ns, t) : args) body)
