217f91e8398a845e1ab8f2afb35484541e05bad1
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2014-08-07 20:26:12 +0200

    Generalize type of desugarArgs.

--- src-lib/PTS/Syntax/Term.hs
+++ src-lib/PTS/Syntax/Term.hs
@@ -104,7 +104,7 @@ data TermStructure alpha
 -- to a series of nested single argument binders:
 --
 -- > lambda x1 : e1 . lambda x2 : e2 . lambda x3 : e2 . e
-desugarArgs :: (Name -> Term -> Term -> Term) -> [([Name], Term)] -> Term -> Term
+desugarArgs :: (Name -> a -> b -> b) -> [([Name], a)] -> b -> b
 desugarArgs mk [] body = body
 desugarArgs mk (([], _) : args) body = desugarArgs mk args body
 desugarArgs mk (((n : ns), t) : args) body = mk n t (desugarArgs mk ((ns, t) : args) body) 
