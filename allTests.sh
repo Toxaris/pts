5ab6bb628adfeedd0076594fbb58b32e81f5bb9c
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-02-07 11:50:36 +0100

    allTests.sh: Increase maximum-unsuitable-generated-tests (fix #134)

--- allTests.sh
+++ allTests.sh
@@ -12,7 +12,7 @@ $CABAL build
 
 $CABAL sdist
 
-dist/build/tests/tests --hide-successes --maximum-generated-tests=1000 --jxml=junit-log.xml
+dist/build/tests/tests --hide-successes --maximum-generated-tests=10000 --maximum-unsuitable-generated-tests=10000 --jxml=junit-log.xml
 
 nameBase=`runhaskell src-tools/package-info.hs --package`
 $CABAL install dist/$nameBase.tar.gz --enable-tests
