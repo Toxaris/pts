abbaec64dbc05769c23ca80138e8eaa10689b500
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-02-08 15:44:06 +0100

    Make pts-nbe compile again
    I'm not sure ignoring the sort in the Pi cases is the best idea, but it
    looks superficially correct to me.
    
    I've refactored the code for the changes in
    3d3be1db5fcdee2171466808c38df524fbf1287c (which dropped asSort), so this
    refactoring should preserve the old meaning -- though it's not fully
    clear to me what this code should do.

--- src-exec/pts-nbe.hs
+++ src-exec/pts-nbe.hs
@@ -12,12 +12,12 @@ f e = [pts| lambda e : $se . lambda R : * -> * .
   se = strip e
   rr t = case structure t of
     Pos _ t -> rr t
-    Pi n t1 t2 -> case asSort (typeOf t1) of
-      Term -> [pts| rrFun R
+    Pi n t1 t2 _ -> case sortOf t1 of
+      Just (C 1) -> [pts| rrFun R
                       $(strip t1) $(cv t1)
                       $(strip t2) $(cv t2)
                       $(rr t1) $(rr t2) |]
-      Type -> [pts| rrAll R $(strip t1)
+      Just (C 2) -> [pts| rrAll R $(strip t1)
                       (lambda $n : $(strip t1) . $(strip t2))
                       (lambda $n : $(strip t1) . $(cv t2))
                       (lambda $n : $(strip t1) . $(rr t2)) |]
@@ -25,17 +25,17 @@ f e = [pts| lambda e : $se . lambda R : * -> * .
 
   cv t = case structure t of
     Pos _ t -> cv t
-    Pi n t1 t2 -> case asSort (typeOf t1) of
-      Term -> [pts| $(cv t1) -> $(cv t2) |]
-      Type -> [pts| Pi $n : $(strip t1) . $(cv t2) |]
+    Pi n t1 t2 _ -> case sortOf t1 of
+      Just (C 1) -> [pts| $(cv t1) -> $(cv t2) |]
+      Just (C 2) -> [pts| Pi $n : $(strip t1) . $(cv t2) |]
     _ -> [pts| Mono R $(strip t) |]
 
   lf e te = case structure te of
     Pos _ te -> lf e te
-    Pi n t1 t2 -> case asSort (typeOf t1) of
-      Term -> let  body = [pts| $e $(mkVar n) |]
+    Pi n t1 t2 _ -> case sortOf t1 of
+      Just (C 1) -> let  body = [pts| $e $(mkVar n) |]
                 in [pts| lambda $n : $(cv t1) . $(lf body t2) |]
-      Type -> let  -- TODO support typeOf t1 /= *
+      Just (C 2) -> let  -- TODO support typeOf t1 /= *
                    body = [pts| $e (Mono R $(mkVar n)) |]
                 in [pts| lambda $n : $(strip t1) . $(lf body t2) |]
     other -> [pts| $e |]
