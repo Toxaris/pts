26bf9cf68598faaeef0ed3dcc8dc49a73d7c4cff
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-02-08 15:46:41 +0100

    Make pts-quote compile again (not working)
    Adapt pts-quote, similarly to pts-nbe in
    abbaec64dbc05769c23ca80138e8eaa10689b500. However, here we have a
    type-directed transformation, so we need to reify back types to continue
    our traversal --- and we need an environment for that!
    
    To make the code compile easily, I used the empty environment; however,
    since this is quoting (not normalization), I believe we want an
    environment that residualizes every variable (that is, such that `n`
    maps to `ResidualVar n` for every used `n`).
    
    However, writing that code without a testcase sounds like a bad idea, so
    for now I'll punt.

--- src-exec/pts-quote.hs
+++ src-exec/pts-quote.hs
@@ -4,15 +4,19 @@ module Main where
 import PTS.Transform
 import PTS.QuasiQuote
 import PTS.Syntax (C (C))
+import PTS.Dynamics.Evaluation
 
 main = transform quote
 
+quote :: TypedTerm Eval -> Term
 quote t = case sortOf t of
-  Term -> quoteTerm t
-  Type -> quoteType t
+  Just (C 1) -> quoteTerm t
+  Just (C 2) -> quoteType t
 
+quoteType :: TypedTerm Eval -> Term
 quoteType t = strip t
 
+quoteTerm :: TypedTerm Eval -> Term
 quoteTerm t = [pts|lambda $repName   :  * -> * .
                    lambda $appName   :  Pi A : * . Pi B : * .
                                           $rep (A -> B) -> $rep A -> $rep B .
@@ -39,12 +43,12 @@ quoteTerm t = [pts|lambda $repName   :  * -> * .
 
   q t = case structure t of
     Var n ->  mkVar n
-    App t1 t2 -> case structure (typeOf t1) of
-      Pi n t3 t4 -> case sortOf t2 of
-        Term  ->  [pts| $app  $(strip t3) $(strip t4) $(q t1) $(q t2) |]
-        Type  ->  [pts| $tapp $(strip t3) (lambda $n : $(strip t3) . $(strip t4)) $(q t1) $(q t2) |]
-    Lam n1 t1 t2 -> case structure (typeOf t) of
-      Pi n2 t3 t4 -> case asSort (typeOf t1) of
-        Term  -> [pts| $abs  $(strip t3) $(strip t4) (lambda $n1 : $rep $(strip t1) . $(q t2)) |]
-        Type  -> [pts| $tabs $(strip t3) (lambda $n2 : $(strip t3) . $(strip t4)) (lambda $n1 : $(strip t1) . $(q t2)) |]
+    App t1 t2 -> case structure (runEval [] {- XXX -} . reify $ typeOf t1) of
+      Pi n t3 t4 _ -> case sortOf t2 of
+        Just (C 1)  ->  [pts| $app  $(strip t3) $(strip t4) $(q t1) $(q t2) |]
+        Just (C 2)  ->  [pts| $tapp $(strip t3) (lambda $n : $(strip t3) . $(strip t4)) $(q t1) $(q t2) |]
+    Lam n1 t1 t2 -> case structure (runEval [] {- XXX -} . reify $ typeOf t) of
+      Pi n2 t3 t4 _ -> case sortOf t1 of
+        Just (C 1)  -> [pts| $abs  $(strip t3) $(strip t4) (lambda $n1 : $rep $(strip t1) . $(q t2)) |]
+        Just (C 2)  -> [pts| $tabs $(strip t3) (lambda $n2 : $(strip t3) . $(strip t4)) (lambda $n1 : $(strip t1) . $(q t2)) |]
     Pos p t -> q t
