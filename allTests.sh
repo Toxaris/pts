04a463e94f6554d4d05e463e94624c0d5c6be4be
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-02-08 11:15:31 +0100

    Travis: run allTests.sh in a different way
    - Split allTests.sh into mostTests.sh and the rest.
    
    - In .travis.yml, call mostTests.sh and then do the last step without
      using runhaskell (and avoiding the library compatibility problems) --
      this is less robust, but will work for an originally empty dist/,
      something we can assume on Travis.

--- allTests.sh
+++ allTests.sh
@@ -1,18 +1,5 @@
 #!/bin/sh -x
-[ -z "$CABAL" ] && CABAL=cabal
-
-set -e
-
-$CABAL clean
-$CABAL check
-
-$CABAL install --only-dependencies --enable-tests
-$CABAL configure --enable-tests
-$CABAL build
-
-$CABAL sdist
-
-dist/build/tests/tests --hide-successes --maximum-generated-tests=10000 --maximum-unsuitable-generated-tests=10000 --jxml=junit-log.xml
+. mostTests.sh
 
 nameBase=`runhaskell src-tools/package-info.hs --package`
 $CABAL install dist/$nameBase.tar.gz --enable-tests
